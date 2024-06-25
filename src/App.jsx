3d511ffdf002eafe66e7dc180fcafaa44111f1d5
gokulprasanthb <gokulprasanthbm@gmail.com>
2024-06-25 20:54:29 +0530

    tailwind css add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <section>
+      <div>
+        <h1 className='text-green-700 text-3xl'>hello world</h1>
+      </div>
+    </section>
+    
+  )
+}
+
+export default App
