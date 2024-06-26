9a67fa1eead26834f964c0303493af5f42ecb075
gokulprasanthb <gokulprasanthbm@gmail.com>
2024-06-26 21:56:35 +0530

    hero section add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,16 @@ const Header = () => {
         <a href="/" className='pl-10 font-bold'>GOKUL</a>
         <nav className='hidden md:block'>
             <ul className='flex flex-row space-x-10 pr-20 '>
-                <li className='hover:text-indigo-950'><a href="/">About</a></li>
-                <li className='hover:text-indigo-950'><a href="/">Projects</a></li>
-                <li className='hover:text-indigo-950'><a href="/">Contact</a></li>
+                <li className='hover:text-stone-700'><a href="/">About</a></li>
+                <li className='hover:text-stone-700'><a href="/">Projects</a></li>
+                <li className='hover:text-stone-700'><a href="/">Contact</a></li>
             </ul>
         </nav>
-        {toggleMenu && <nav className='items-end block md:hidden fixed top-14 right-0 w-1/3 h-full bg-indigo-95 0 text-white text-start px-8'>
+        {toggleMenu && <nav className='items-end block md:hidden fixed top-14 right-0 w-1/3 h-full bg-indigo-95     0 text-white text-start px-8'>
             <ul className='flex flex-col space-y-8 pt-8 '>
-                <li className='hover:text-indigo-950'><a href="/">About</a></li>
-                <li className='hover:text-indigo-950'><a href="/">Projects</a></li>
-                <li className='hover:text-indigo-950'><a href="/">Contact</a></li>
+                <li className='hover:text-stone-700'><a href="/">About</a></li>
+                <li className='hover:text-stone-700'><a href="/">Projects</a></li>
+                <li className='hover:text-stone-700'><a href="/">Contact</a></li>
             </ul>
         </nav>}
         <button onClick={() => setToggleMenu(!toggleMenu)} className='block md:hidden'><Bars3Icon className=' max-md:text-white h-8 text-lg font-bold pr-4'/></button>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import image from '../assets/myImage.jpeg'
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+
+
+const Hero = () => {
+  return (
+    <section className='flex flex-row justify-center pl-40 px-28 pt-32  max-md:flex-col max-md:px-5 max-md:pt-5'>
+        <div className='w-1/2 mt-24 text-5xl space-y-5 text-indigo-900 max-md:w-full max-md:text-3xl max-md:text-center'>
+            <h1>Hi, </h1>
+            <h1>I'm <strong>Gokul Prasanth </strong></h1>
+            <h3 className='text-3xl text-stone-700'>A Full stack Developer</h3>
+            <div className='text-xl font-medium space-x-6 pt-2 '>
+                <button className='bg-indigo-900 text-white w-36 h-14 hover:bg-white hover:text-indigo-900 hover:border-2 hover:border-indigo-900 rounded-sm'>Download CV</button>
+                <button className='border-2 border-indigo-900 w-36 h-14 hover:bg-indigo-900 hover:text-white rounded-sm'>Connect</button>
+            </div>
+            <div className='flex space-x-3 pt-2 max-md:hidden'>
+                <a href="/"><FaLinkedin className='w-8 h-8 hover:text-stone-700'/></a>
+                <a href="/"><FaSquareXTwitter className='w-8 h-8 hover:text-stone-700'/></a>
+                <a href="/"><FaFacebook className='w-8 h-8 hover:text-stone-700'/></a>
+                <a href="/"><FaInstagramSquare className='w-8 h-8 hover:text-stone-700'/></a>
+            </div>
+        </div>
+
+        <div className='w-1/2 pl-14 max-md:w-full max-md:pl-0'>
+            <img src={image} alt="" className='h-96 rounded-lg max-md:pl-6 max-md:pt-10' />
+        </div>
+    </section>
+  )
+}
+
+export default Hero
