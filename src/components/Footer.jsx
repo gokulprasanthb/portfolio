01dd0ca55ce2ed012670bb42ffeaab620cf95131
gokulprasanth.me21 <gokulprasanth.me21@bitsathy.ac.in>
2025-05-07 11:52:36 +0530

    Footer modifiy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import myLogo from '../assets/GPlogoo.png'
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaFacebook } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
 
 const Footer = () => {
