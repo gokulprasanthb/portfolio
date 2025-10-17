208f472efa5af9a47ff0ceb59fc5db7bf5a54c8b
gokulprasanthb <gokulprasnthb@gmail.com>
2025-10-17 12:10:45 +0530

    Resume Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import myLogo from '../assets/GPlogoo.png'
 
 const Header = () => {
     const [toggleMenu, setToggleMenu] = useState(false)
+    
     return (
         <header id='header' className='w-full bg-white fixed shadow-lg shadow-stone-100 flex justify-between py-5 font-semibold text-indigo-950 text-lg max-md:bg-indigo-950 max-md:text-white max-md:shadow-none md:px-20 max-md:w-full max-md:pt-6'>
             <a href="/" className='pl-10 md:pl-32'><img className='w-12 h-10 max-md:w-8 max-md:h-8 max-md:rounded-full max-md:bg-white max-md:p-1' src={myLogo} alt="logo" /></a>
